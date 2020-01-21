863024c6ce6cce9ade9829b5d0b0d96c4677839a
ichwn <ichwanarif26@gmail.com>
2020-01-21 21:55:40 +0700

    add dynamic options

--- canvas.js
+++ canvas.js
@@ -11,36 +11,6 @@ window.addEventListener("resize", () => {
   initParticle();
 });
 
-// context.fillStyle = "red";
-// context.fillRect(100, 100, 100, 100);
-// context.fillStyle = "blue";
-// context.fillRect(200, 200, 100, 100);
-// context.fillStyle = "yellow";
-// context.fillRect(300, 300, 100, 100);
-
-//line
-// context.beginPath();
-// context.moveTo(50, 300);
-// context.lineTo(300, 100);
-// context.strokeStyle = "green";
-// context.lineTo(400, 300);
-// context.stroke();
-
-//arc
-// context.beginPath();
-// context.strokeStyle = "orange";
-// context.arc(300, 300, 30, 0, Math.PI * 2, false);
-// context.stroke();
-// let color = ["black", "brown", "orange"];
-// for (let index = 0; index < 3; index++) {
-//   let x = Math.random() * window.innerWidth;
-//   let y = Math.random() * window.innerHeight;
-//   context.beginPath();
-//   context.arc(x, y, 30, 0, Math.PI * 2, false);
-//   context.strokeStyle = color[index];
-//   context.stroke();
-// }
-
 let mouse = {
   x: undefined,
   y: undefined
@@ -53,18 +23,21 @@ canvas.addEventListener("mousemove", event => {
 let = maxRadius = 50;
 let colorBulets = ["#ffaa33", "#99ffaa", "#80ff00", "#4411aa", "#ff1100"];
 
-//animated
-function Circle(x, y, dx, dy, radius) {
-  this.x = x;
-  this.y = y;
-  this.dx = dx;
-  this.dy = dy;
-  this.radius = radius;
-  this.minRadius = radius;
-  this.generateColor =
-    colorBulets[Math.floor(Math.random() * colorBulets.length)];
+//Draw circles
+class Circle {
+  constructor(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.minRadius = radius;
+    this.generateColor =
+      colorBulets[Math.floor(Math.random() * colorBulets.length)];
+  }
+  
 
-  this.draw = function() {
+  draw = function() {
     context.beginPath();
     // context.strokeText("DVD", this.x - 10, this.y + 5);
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
@@ -76,7 +49,7 @@ function Circle(x, y, dx, dy, radius) {
     return this;
   };
 
-  this.update = function() {
+  update = function() {
     if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
       this.dx = -this.dx;
     }
@@ -102,15 +75,88 @@ function Circle(x, y, dx, dy, radius) {
   };
 }
 
+//toggle menu 
+const toggleMenu = param => {
+  let wrapperMenu = document.querySelector(".options-wrapper")
+  if(param.dataset.toggle === "true") {
+    wrapperMenu.style.transform = "translateX(0px)"
+    param.dataset.toggle = "false"
+    param.innerHTML = "&laquo;"
+  } else {
+    wrapperMenu.style.transform = "translateX(-280px)"
+    param.dataset.toggle = "true"
+    param.innerHTML = "&raquo;"
+  }
+}
+
+
+/**
+ * Dynamic Options
+ */
+let totalParticles =  document.querySelector("#total-particle")
+totalParticles.value = 200
+let circleSize =  document.querySelector("#circle-size")
+circleSize.value = 1
+let isSpeed = document.querySelector("#isSpeed")
+let speedParticle = document.querySelector("#particle-speed")
+speedParticle.value = 3
+let bounceX = document.querySelector("#bounce-x")
+bounceX.value = 3
+let bounceY = document.querySelector("#bounce-y")
+bounceY.value = 3
+
+//add on enter event
+let selectedElements = [totalParticles,circleSize,speedParticle,bounceX,bounceY]
+selectedElements.map(res => {
+  res.addEventListener("keypress", e=> {
+    e.key === "Enter" && initParticle()
+  })
+})
+
+//change bounce type
+const changeBounce = () => {
+  if(isSpeed.checked) {
+    bounceX.style.opacity = 0.5
+    bounceX.disabled = true
+    bounceX.value = 3
+    bounceY.style.opacity = 0.5
+    bounceY.disabled = true
+    bounceY.value = 3
+    speedParticle.style.opacity = 1
+    speedParticle.disabled = false
+  } else {
+    bounceX.style.opacity = 1
+    bounceX.disabled = false
+    bounceY.style.opacity = 1
+    bounceY.disabled = false
+    speedParticle.style.opacity = 0.5
+    speedParticle.disabled = true
+    speedParticle.value = 3
+  }
+}
+
+//type of bounce
+const getType = type => {
+  if(!isSpeed.checked) {
+    if(type === "x") {
+      return bounceX.value
+    } else {
+      return bounceY.value
+    }
+  } else {
+    return speedParticle.value
+  }
+}
+
 var circleArray = [];
 function initParticle() {
   circleArray = [];
-  for (let i = 0; i < 200; i++) {
+  for (let i = 0; i < totalParticles.value; i++) {
     var radius = Math.random() * 5 + 1;
     var x = Math.random() * (innerWidth - radius * 2) + radius;
     var y = Math.random() * (innerHeight - radius * 2) + radius;
-    var dx = (Math.random() - 0.5) * 3;
-    var dy = (Math.random() - 0.5) * 3;
+    var dx = (Math.random() - 0.5) * getType("x");
+    var dy = (Math.random() - 0.5) * getType("y");
     circleArray.push(new Circle(x, y, dx, dy, radius));
   }
 }
